fd3b3f8086db0ab837130f55a4efefc08f70d9a9
ZuXolo BR <155470121+Lucashgdhhyy@users.noreply.github.com>
2025-02-23 18:23:49 -0300

    Update and rename Main loader to Main loader.lua

---
+++ Main loader.lua
@@ -0,0 +1,9 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/Lucashgdhhyy/Durklua/refs/heads/main/Script/Orion/Orion.lua')))()
+local Window = OrionLib:MakeWindow({Name = "Azurion.wtf", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest", IntroText = "by Azurion.wtf"})
+OrionLib:MakeNotification({
+	Name = "Discord Server",
+	Content = "Copied link of discord",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+
